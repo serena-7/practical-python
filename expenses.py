aec33abb6dc10dd6363ce5ba59ed4b3591ba925d
Serena Sorensen <95942845+serena-7@users.noreply.github.com>
2022-05-17 18:46:25 -0600

    part 1 complete

---
+++ expenses.py
@@ -0,0 +1,10 @@
+expenses = [10.50, 8, 5, 15, 20, 5, 3]
+
+total = sum(expenses)
+
+# sum = 0
+
+# for expense in expenses:
+#     sum = sum + expense
+
+print("You spent $", total, " on lunch this week.", sep="")
---
+++ tax_calculator.py
@@ -0,0 +1,7 @@
+from os import TMP_MAX
+
+
+amount = 100
+tax = .06
+total = amount + amount * tax
+print(total)
